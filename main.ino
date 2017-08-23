6bbe4d11b263c1e8450139ad785465ba6ccfd7ff
Kim Tore Jensen <kim@incendio.no>
2017-08-23 12:32:47 +0200

    Implement solid color mode, and parameter changing through rotary encoder

--- main.ino
+++ main.ino
@@ -6,9 +6,9 @@
 #define ENCODER_OPTIMIZE_INTERRUPTS
 #include <Encoder.h>
 
-/* Digital output pins. */
-#define PIN_ROTARY_LEFT 2
-#define PIN_ROTARY_RIGHT 3
+/* Digital IO pins. */
+#define PIN_ROTARY_ONE 3
+#define PIN_ROTARY_TWO 2
 #define PIN_BUTTON 4
 #define PIN_LED 6
 
@@ -16,25 +16,32 @@
 #define NUM_LEDS 60
 
 /* Rotary encoder object */
-Encoder encoder(PIN_ROTARY_LEFT, PIN_ROTARY_RIGHT);
+Encoder encoder(PIN_ROTARY_ONE, PIN_ROTARY_TWO);
 
 /* This variable holds the LED color state. */
 CRGB leds[NUM_LEDS];
 
-/* Define the different variables that can be changed using the rotary wheel. */
-#define VARIABLE_MODE 0
-#define VARIABLE_HUE 1
-#define VARIABLE_SATURATION 2
-#define VARIABLE_VALUE 3
-#define VARIABLE_SPEED 4
-//#define VARIABLE_XXX 5
-//#define VARIABLE_XXX 6
-#define MAX_VARIABLES 7
+/* Define the different parameters that can be changed using the rotary wheel. */
+#define PARAMETER_MODE 0
+#define PARAMETER_HUE 1
+#define PARAMETER_SATURATION 2
+#define PARAMETER_VALUE 3
+#define PARAMETER_SPEED 4
+//#define PARAMETER_XXX 5
+//#define PARAMETER_XXX 6
+#define MAX_PARAMETERS 7
 
-/* Holds the variable that is currently changed using the rotary wheel. */
-uint8_t activeVariable = 0;
+/* Holds the parameter that is currently changed using the rotary wheel. */
+uint8_t activeParameter = PARAMETER_MODE;
 
-/* Rainbow colors, used to signify active variable to the user. */
+/* Holds the values of the different parameters. */
+uint8_t parameters[MAX_PARAMETERS] = { 0, 0, 255, 80, 80, 0, 0 };
+
+/* Maximum values of the different parameters, plus one. Zero denotes that a
+ * variable can stretch the full range of 0-255. */
+uint8_t parameterMax[MAX_PARAMETERS] = { 1, 0, 0, 0, 0, 0, 0 };
+
+/* Rainbow colors, used to signify active parameter to the user. */
 CRGB rainbowColors[] = {
     CHSV(0,   255, 255),
     CHSV(36,  255, 255),
@@ -47,10 +54,7 @@ CRGB rainbowColors[] = {
 
 void setup() {
     FastLED.addLeds<NEOPIXEL, PIN_LED>(leds, NUM_LEDS);
-
-    pinMode(PIN_BUTTON,       INPUT_PULLUP);
-    pinMode(PIN_ROTARY_LEFT,  INPUT_PULLUP);
-    pinMode(PIN_ROTARY_RIGHT, INPUT_PULLUP);
+    pinMode(PIN_BUTTON, INPUT_PULLUP);
 }
 
 /* buttonPressed returns true if the button is pressed, false otherwise. */
@@ -72,29 +76,48 @@ bool buttonChanged() {
     return rval;
 }
 
-void loop() {
+/* rotary_delta returns the number of full steps the rotary encoder has changed. */
+int8_t rotary_delta() {
     int32_t e;
-
-    if (buttonChanged()) {
-        if (buttonPressed()) {
-            activeVariable = (activeVariable + 1) % MAX_VARIABLES;
-            fill_solid(&leds[0], NUM_LEDS, rainbowColors[activeVariable]);
-        } else {
-            fill_solid(&leds[0], NUM_LEDS, CRGB::Black);
-        }
-    }
+    int8_t rval = 0;
 
     e = encoder.read();
-    if (e <= -4) {
-        leds[10] = CHSV(0, 255, 255);
-        e += 4;
-    } else if (e >= 4) {
-        leds[10] = CHSV(120, 255, 255);
-        e -= 4;
-    } else {
-        leds[10] = CRGB::Black;
-    }
+
+    rval = e / 4;
+    e -= (rval * 4);
+
     encoder.write(e);
 
+    return rval;
+}
+
+void modeSolid() {
+    fill_solid(&leds[0], NUM_LEDS, CHSV(
+        parameters[PARAMETER_HUE],
+        parameters[PARAMETER_SATURATION],
+        parameters[PARAMETER_VALUE]
+    ));
+}
+
+void loop() {
+
+    /* Increase active parameter if button is pressed. */
+    if (buttonChanged() && buttonPressed()) {
+        activeParameter = (activeParameter + 1) % MAX_PARAMETERS;
+        fill_solid(&leds[0], NUM_LEDS, rainbowColors[activeParameter]);
+        FastLED.show();
+    }
+
+    /* Pause all activity while button is held down. */
+    if (buttonPressed()) {
+        return;
+    }
+
+    /* Increase or decrease parameter if rotary encoder moved. */
+    parameters[activeParameter] += rotary_delta();
+    parameters[activeParameter] %= parameterMax[activeParameter];
+
+    /* Only one mode for now */
+    modeSolid();
     FastLED.show();
 }
