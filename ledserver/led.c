7ee91cd57ace105ca3b157f3bbc818b6ee713e72
Kim Tore Jensen <kimtjen@gmail.com>
2017-11-07 13:55:24 +0100

    ledserver: merge led server code and udp server code.

--- ledserver/led.c
+++ ledserver/led.c
@@ -5,17 +5,8 @@
  * library and Google Protobuf messages.
  */
 
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <signal.h>
-#include <stdarg.h>
+#include <stdint.h>
 
 #include "rpi_ws281x/version.h"
 #include "rpi_ws281x/ws2811.h"
@@ -30,14 +21,6 @@
 #define TARGET_FREQ             WS2811_TARGET_FREQ
 
 static ws2811_t ledstring;
-
-static uint8_t running = 1;
-
-void ledstrip_clear(void) {
-    for (int i = 0; i < LED_COUNT; i++) {
-        ledstring.channel[0].leds[i] = 0;
-    }
-}
 
 int ledstrip_init() {
     ledstring.freq = TARGET_FREQ,
@@ -61,17 +44,14 @@ int ledstrip_init() {
 }
 
 void ledstrip_finish() {
-    ledstrip_clear();
     ws2811_render(&ledstring);
     ws2811_fini(&ledstring);
 }
 
-int ledstrip_render() {
-    ws2811_return_t ret;
-    if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS) {
-        fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
+void ledstrip_clear(void) {
+    for (int i = 0; i < LED_COUNT; i++) {
+        ledstring.channel[0].leds[i] = 0;
     }
-    return ret;
 }
 
 void ledstrip_assign(uint32_t led, uint32_t value) {
@@ -81,41 +61,10 @@ void ledstrip_assign(uint32_t led, uint32_t value) {
     ledstring.channel[0].leds[led] = value;
 }
 
-static void ctrl_c_handler(int signum)
-{
-    (void)(signum);
-    running = 0;
-}
-
-static void setup_handlers(void)
-{
-    struct sigaction sa;
-    sa.sa_handler = ctrl_c_handler;
-
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGTERM, &sa, NULL);
-}
-
-int main(int argc, char *argv[])
-{
-    ledstrip_init();
-
-    setup_handlers();
-
-    while (running)
-    {
-        ledstrip_assign(29, 127);
-        ledstrip_assign(30, 255);
-        ledstrip_render();
-
-        // 15 frames /sec
-        usleep(1000000 / 15);
+int ledstrip_render() {
+    ws2811_return_t ret;
+    if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS) {
+        fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
     }
-
-    ledstrip_clear();
-    ledstrip_finish();
-
-    printf ("\n");
-
-    return 0;
+    return ret;
 }
--- ledserver/server.c
+++ ledserver/server.c
@@ -5,65 +5,102 @@
  */
 
 #include <errno.h>
-#include <string.h>
-#include <unistd.h>
 #include <netdb.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
-
-#include <stdio.h>
+#include <signal.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-#define HOST "0.0.0.0"
+#include "led.h"
+
+#define HOST 0
 #define PORT "1230"
 
-void die(const char * fmt, ...) {
-    va_list valist;
-    vprintf(fmt, valist);
-    exit(1);
+static uint8_t running = 1;
+
+static void ctrl_c_handler(int signum)
+{
+    (void)(signum);
+    running = 0;
 }
 
-void serve() {
+static void setup_handlers(void)
+{
+    struct sigaction sa;
+    sa.sa_handler = ctrl_c_handler;
+
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+}
+
+int main() {
     int fd;
     int err;
-
     struct addrinfo hints;
-    struct addrinfo* res=0;
+    struct addrinfo* ai = 0;
+    char buffer[548];
+    struct sockaddr_storage src_addr;
+    socklen_t src_addr_len = sizeof(src_addr);
+    ssize_t received;
 
+    // create socket options for an UDP socket.
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_protocol = 0;
     hints.ai_flags = AI_PASSIVE|AI_ADDRCONFIG;
 
-    err = getaddrinfo(HOST, PORT, &hints, &res);
+    err = getaddrinfo(HOST, PORT, &hints, &ai);
     if (err != 0) {
-        die("failed to resolve local socket address (err=%d)",err);
+        fprintf(stderr, "failed to resolve local socket address (err=%d)",err);
+        return 1;
     }
 
-    fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    // create an UDP socket.
+    fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
     if (fd==-1) {
-        die("%s",strerror(errno));
+        fprintf(stderr, "%s", strerror(errno));
+        return 1;
     }
 
-    freeaddrinfo(res);
-
-    if (bind(fd,res->ai_addr,res->ai_addrlen)==-1) {
-        die("%s", strerror(errno));
+    // bind the UDP socket.
+    if (bind(fd, ai->ai_addr, ai->ai_addrlen) == -1) {
+        fprintf(stderr, "%s", strerror(errno));
+        return 1;
     }
 
-    char buffer[549];
-    struct sockaddr_storage src_addr;
-    socklen_t src_addr_len=sizeof(src_addr);
-    ssize_t count=recvfrom(fd,buffer,sizeof(buffer),0,(struct sockaddr*)&src_addr,&src_addr_len);
-    if (count==-1) {
-        die("%s",strerror(errno));
-    } else if (count==sizeof(buffer)) {
-        //warn("datagram too large for buffer: truncated");
-    } else {
-        //handle_datagram(buffer,count);
+    freeaddrinfo(ai);
+    setup_handlers();
+
+    // socket is set up; initialize the LEDs.
+    ledstrip_init();
+    ledstrip_clear();
+    ledstrip_render();
+
+    uint32_t count = 0;
+    while (running) {
+        received = recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&src_addr, &src_addr_len);
+        if (received == -1) {
+            fprintf(stderr, "%s", strerror(errno));
+        } else if (received == sizeof(buffer)) {
+            printf("datagram too large for buffer: truncated\n");
+        } else {
+            printf("received datagram of size %d\n", received);
+            ledstrip_assign(count, 255*(count+1));
+            ledstrip_render();
+            ++count;
+        }
     }
 
+    ledstrip_clear();
+    ledstrip_finish();
     close(fd);
+
+    printf("\n");
+
+    return 0;
 }
