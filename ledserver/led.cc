e907cb9f8dad1c68a36e980f1bbadb080cccbd1d
Kim Tore Jensen <kimtjen@gmail.com>
2017-11-07 11:36:38 +0100

    ledserver: initial commit; drives a strip of 60 LEDs directly on BCM18.

---
+++ ledserver/led.cc
@@ -0,0 +1,151 @@
+/*
+ * LEDServer by Kim Tore Jensen.
+ *
+ * This program drives LEDs from a Raspberry Pi Zero W using the rpi_ws281x
+ * library and Google Protobuf messages.
+ *
+ * Parts of code copyright (c) 2014 Jeremy Garff <jer @ jers.net> under the following license:
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ *     1.  Redistributions of source code must retain the above copyright notice, this list of
+ *         conditions and the following disclaimer.
+ *     2.  Redistributions in binary form must reproduce the above copyright notice, this list
+ *         of conditions and the following disclaimer in the documentation and/or other materials
+ *         provided with the distribution.
+ *     3.  Neither the name of the owner nor the names of its contributors may be used to endorse
+ *         or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+static char VERSION[] = "XX.YY.ZZ";
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <signal.h>
+#include <stdarg.h>
+
+#include "rpi_ws281x/version.h"
+#include "rpi_ws281x/ws2811.h"
+
+#define TARGET_FREQ             WS2811_TARGET_FREQ
+#define GPIO_PIN                18
+#define DMA                     5
+#define STRIP_TYPE              WS2811_STRIP_RGB        // WS2812/SK6812RGB integrated chip+leds
+
+#define LED_COUNT               60
+
+ws2811_t ledstring;
+ws2811_led_t *strip;
+
+static uint8_t running = 1;
+
+void init() {
+    ledstring.freq = TARGET_FREQ,
+    ledstring.dmanum = DMA,
+    ledstring.channel[0].gpionum = GPIO_PIN;
+    ledstring.channel[0].count = LED_COUNT;
+    ledstring.channel[0].invert = 0;
+    ledstring.channel[0].brightness = 80;
+    ledstring.channel[0].strip_type = STRIP_TYPE;
+    ledstring.channel[1].gpionum = 0;
+    ledstring.channel[1].count = 0;
+    ledstring.channel[1].invert = 0;
+    ledstring.channel[1].brightness = 0;
+}
+
+void strip_draw(void) {
+    for (int i = 0; i < LED_COUNT; i++) {
+        strip[i] = 128;
+    }
+}
+
+void strip_render(void) {
+    for (int i = 0; i < LED_COUNT; i++) {
+        ledstring.channel[0].leds[i] = strip[i];
+    }
+}
+
+void strip_clear(void) {
+    for (int i = 0; i < LED_COUNT; i++) {
+        strip[i] = 0;
+    }
+}
+
+static void ctrl_c_handler(int signum)
+{
+    (void)(signum);
+    running = 0;
+}
+
+static void setup_handlers(void)
+{
+    struct sigaction sa;
+    sa.sa_handler = ctrl_c_handler;
+
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+}
+
+int main(int argc, char *argv[])
+{
+    ws2811_return_t ret;
+
+    init();
+
+    sprintf(VERSION, "%d.%d.%d", VERSION_MAJOR, VERSION_MINOR, VERSION_MICRO);
+
+    strip = (ws2811_led_t*) malloc(sizeof(ws2811_led_t) * LED_COUNT);
+
+    setup_handlers();
+
+    if ((ret = ws2811_init(&ledstring)) != WS2811_SUCCESS)
+    {
+        fprintf(stderr, "ws2811_init failed: %s\n", ws2811_get_return_t_str(ret));
+        return ret;
+    }
+
+    while (running)
+    {
+        strip_draw();
+        strip_render();
+
+        if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS)
+        {
+            fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
+            break;
+        }
+
+        // 15 frames /sec
+        usleep(1000000 / 15);
+    }
+
+    strip_clear();
+    strip_render();
+    ws2811_render(&ledstring);
+
+    ws2811_fini(&ledstring);
+
+    printf ("\n");
+    return ret;
+}
