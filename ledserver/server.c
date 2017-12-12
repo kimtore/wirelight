10947a5f2db2aa7998785326e7887a2a2cda5fdd
Kim Tore Jensen <kimtjen@gmail.com>
2017-12-12 21:24:12 +0100

    ledserver: use ZeroMQ instead of UDP packets.

--- ledserver/server.c
+++ ledserver/server.c
@@ -4,22 +4,13 @@
  * This is the server component. It listens on an UDP socket for Protobuf messages.
  */
 
-#include <errno.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <signal.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include <zmq.h>
 
 #include "led.h"
 #include "pb.pb-c.h"
 
-#define HOST 0
-#define PORT "1230"
+#define ADDRESS "tcp://0.0.0.0:1230"
 
 static uint8_t running = 1;
 
@@ -38,46 +29,33 @@ static void setup_handlers(void)
     sigaction(SIGTERM, &sa, NULL);
 }
 
+static LED * recv_zmq_led(void * socket)
+{
+    char buffer[64];
+    int size = zmq_recv(socket, buffer, 64, 0);
+    if (size == -1) {
+        return NULL;
+    }
+    buffer[size] = '\0';
+    return led__unpack(NULL, size, (const uint8_t *)buffer);
+}
+
 int main() {
-    int                     fd;
-    int                     err;
-    struct addrinfo         hints;
-    struct addrinfo *       ai = 0;
-    char                    buffer[548];
-    struct sockaddr_storage src_addr;
-    socklen_t               src_addr_len = sizeof(src_addr);
-    ssize_t                 received;
-    uint64_t                count = 0;
-    LED *                   led;
+    uint64_t count = 0;
+    LED *    led;
 
-    // create socket options for an UDP socket.
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_protocol = 0;
-    hints.ai_flags = AI_PASSIVE|AI_ADDRCONFIG;
-
-    err = getaddrinfo(HOST, PORT, &hints, &ai);
-    if (err != 0) {
-        fprintf(stderr, "failed to resolve local socket address (err=%d)",err);
-        return 1;
-    }
-
-    // create an UDP socket.
-    fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
-    if (fd==-1) {
-        fprintf(stderr, "%s", strerror(errno));
-        return 1;
-    }
-
-    // bind the UDP socket.
-    if (bind(fd, ai->ai_addr, ai->ai_addrlen) == -1) {
-        fprintf(stderr, "%s", strerror(errno));
-        return 1;
-    }
-
-    freeaddrinfo(ai);
+    // set up signal handlers.
     setup_handlers();
+
+    // set up ZeroMQ context and socket.
+    void *context = zmq_ctx_new();
+    void *subscriber = zmq_socket(context, ZMQ_SUB);
+    int rc = zmq_bind(subscriber, ADDRESS);
+    assert (rc == 0);
+
+    // no filtering.
+    rc = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0);
+    assert (rc == 0);
 
     // socket is set up; initialize the LEDs.
     ledstrip_init();
@@ -85,31 +63,26 @@ int main() {
     ledstrip_render();
 
     while (running) {
-        received = recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&src_addr, &src_addr_len);
-        ++count;
-        if (received == -1) {
-            fprintf(stderr, "%s", strerror(errno));
-        } else if (received == sizeof(buffer)) {
-            fprintf(stderr, "datagram too large for buffer: truncated\n");
-        } else {
-            led = led__unpack(NULL, received, (const uint8_t *)buffer);
-            if (led == NULL) {
-                fprintf(stderr, "invalid protobuf message with size %d received; discarding.\n", received);
-                continue;
-            }
-            ledstrip_assign(led->index, led->rgb);
-            if (led->render) {
-                ledstrip_render();
-            }
-            led__free_unpacked(led, NULL);
+        led = recv_zmq_led(subscriber);
+        if (led == NULL) {
+            fprintf(stderr, "invalid protobuf message received; discarding.\n");
+            continue;
         }
+        ledstrip_assign(led->index, led->rgb);
+        if (led->render) {
+            ledstrip_render();
+        }
+        led__free_unpacked(led, NULL);
+        ++count;
     }
+
+    zmq_close(subscriber);
+    zmq_ctx_destroy(context);
 
     ledstrip_clear();
     ledstrip_finish();
-    close(fd);
 
-    printf("\nProcessed %llu datagrams.\n", count-1);
+    printf("\nProcessed %llu LED updates.\n", count);
 
     return 0;
 }
