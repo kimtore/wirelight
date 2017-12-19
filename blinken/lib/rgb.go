cf0a88eff0ed1c2d644d2bd4559e9b547bca7bb7
Kim Tore Jensen <kimtjen@gmail.com>
2017-12-19 21:20:55 +0100

    blinken: add library function for converting a non-alpha color to a colorful color.

--- blinken/lib/rgb.go
+++ blinken/lib/rgb.go
@@ -3,6 +3,8 @@ package lib
 import (
 	"image/color"
 	"math"
+
+	colorful "github.com/lucasb-eyer/go-colorful"
 )
 
 const x = math.Pi / 180
@@ -11,6 +13,17 @@ const x = math.Pi / 180
 func RGBA(c color.Color) uint32 {
 	r, g, b, a := c.RGBA()
 	return (a & 0xff << 24) | (r & 0xff << 16) | (g & 0xff << 8) | (b & 0xff)
+}
+
+// MakeColor converts a Color to the corresponding Colorful type.
+// This is a workaround for https://github.com/lucasb-eyer/go-colorful/issues/21.
+func MakeColor(c color.Color) colorful.Color {
+	r, g, b, _ := c.RGBA()
+	return colorful.Color{
+		float64(r) / 65535.0,
+		float64(g) / 65535.0,
+		float64(b) / 65535.0,
+	}
 }
 
 // Rad converts degrees to radians.
--- blinken/lib/rgb_test.go
+++ blinken/lib/rgb_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/ambientsound/wirelight/blinken/lib"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var rgbTests = []struct {
@@ -26,5 +27,35 @@ func TestRGB(t *testing.T) {
 	for _, test := range rgbTests {
 		i := lib.RGBA(test.c)
 		assert.Equal(t, test.i, i)
+	}
+}
+
+// Test that the color conversion function works as expected.
+func TestMakeColor(t *testing.T) {
+	for _, test := range rgbTests {
+		c := lib.MakeColor(test.c)
+		rt, gt, bt, _ := test.c.RGBA()
+		rc, gc, bc, _ := c.RGBA()
+		assert.Equal(t, rt, rc)
+		assert.Equal(t, gt, gc)
+		assert.Equal(t, bt, bc)
+	}
+}
+
+// Test that the color conversion function works as expected.
+func TestMakeColor2(t *testing.T) {
+	var r, g, b, a uint8
+	for r = 0; r < 255; r++ {
+		for g = 0; g < 255; g++ {
+			for b = 0; b < 255; b++ {
+				ct := color.RGBA{r, g, b, a}
+				cc := lib.MakeColor(ct)
+				rt, gt, bt, _ := ct.RGBA()
+				rc, gc, bc, _ := cc.RGBA()
+				require.Equal(t, rt, rc)
+				require.Equal(t, gt, gc)
+				require.Equal(t, bt, bc)
+			}
+		}
 	}
 }
