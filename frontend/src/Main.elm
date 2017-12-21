002041db9ba198bb342112f00871a39dcab42f5a
Kim Tore Jensen <kimtjen@gmail.com>
2017-12-21 21:34:22 +0100

    Create a simple hue/chroma/luminance Elm application.

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,43 +1,106 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, text, div, h1, h2, input)
+import Html.Attributes exposing (src, type_, class, min, max, value)
+import Html.Events exposing (onInput)
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { hue : ColorParam
+    , chroma : ColorParam
+    , luminance : ColorParam
+    }
+
+
+type alias ColorParam =
+    Int
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { hue = 0
+      , chroma = 0
+      , luminance = 0
+      }
+    , Cmd.none
+    )
 
 
 
 ---- UPDATE ----
 
 
+type HclParam
+    = Hue
+    | Chroma
+    | Luminance
+
+
 type Msg
     = NoOp
+    | HclChange HclParam String
+
+
+colorFraction : ColorParam -> Float
+colorFraction c =
+    toFloat c / 65535
+
+
+colorFractionString : ColorParam -> String
+colorFractionString c =
+    String.left 6 (toString (colorFraction c))
+
+
+zint : String -> ColorParam
+zint s =
+    Result.withDefault 0 (String.toInt s)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        HclChange Hue s ->
+            ( { model | hue = zint s }, Cmd.none )
+
+        HclChange Chroma s ->
+            ( { model | chroma = zint s }, Cmd.none )
+
+        HclChange Luminance s ->
+            ( { model | luminance = zint s }, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
+slider : String -> HclParam -> ColorParam -> Html Msg
+slider title hclparam colorvalue =
+    div [ class "slider" ]
+        [ h2 [] [ text (title ++ ": " ++ colorFractionString colorvalue) ]
+        , input
+            [ type_ "range"
+            , Html.Attributes.min "0"
+            , Html.Attributes.max "65535"
+            , value (toString colorvalue)
+            , onInput (HclChange hclparam)
+            ]
+            []
+        ]
+
+
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ h1 [] [ text "Any Colour You Like" ]
+        , slider "Hue" Hue model.hue
+        , slider "Chroma" Chroma model.chroma
+        , slider "Luminance" Luminance model.luminance
         ]
 
 
