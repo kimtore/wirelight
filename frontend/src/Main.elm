00dfad31b43313f5f7a198f906973b32b673528e
Kim Tore Jensen <kimtjen@gmail.com>
2017-12-22 00:28:05 +0100

    frontend: use websockets to send color updates.

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -3,6 +3,8 @@ module Main exposing (..)
 import Html exposing (Html, text, div, h1, h2, input)
 import Html.Attributes exposing (src, type_, class, min, max, value)
 import Html.Events exposing (onInput)
+import Json.Encode
+import WebSocket
 
 
 ---- MODEL ----
@@ -59,17 +61,31 @@ zint s =
     Result.withDefault 0 (String.toInt s)
 
 
+colorObject : Model -> Json.Encode.Value
+colorObject m =
+    Json.Encode.object
+        [ ( "hue", Json.Encode.int m.hue )
+        , ( "chroma", Json.Encode.int m.chroma )
+        , ( "luminance", Json.Encode.int m.luminance )
+        ]
+
+
+sendColors : Model -> Cmd Msg
+sendColors model =
+    WebSocket.send "ws://localhost:8011/" (toString (Json.Encode.encode 0 (colorObject model)))
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         HclChange Hue s ->
-            ( { model | hue = zint s }, Cmd.none )
+            ( { model | hue = zint s }, sendColors model )
 
         HclChange Chroma s ->
-            ( { model | chroma = zint s }, Cmd.none )
+            ( { model | chroma = zint s }, sendColors model )
 
         HclChange Luminance s ->
-            ( { model | luminance = zint s }, Cmd.none )
+            ( { model | luminance = zint s }, sendColors model )
 
         NoOp ->
             ( model, Cmd.none )
