3d2c0c59dc78919122b0804ab902904e1441f604
Kim Tore Jensen <kimtjen@gmail.com>
2024-09-27 14:41:06 +0200

    nuled: Compile IDF code

--- nuled/src/main.rs
+++ nuled/src/main.rs
@@ -1,8 +1,11 @@
 #![no_std]
 #![no_main]
 
+use esp_idf_hal::prelude::{FromValueType, Peripherals};
+use esp_idf_hal::spi::Dma;
+
 #[no_mangle]
-fn main() {
+unsafe fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -11,4 +14,19 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    let peripherals = Peripherals::take().unwrap();
+    let driver = esp_idf_hal::spi::SpiDriver::new_without_sclk(
+        peripherals.spi2,
+        peripherals.pins.gpio10,
+        Option::<esp_idf_hal::gpio::AnyIOPin>::None,
+        &esp_idf_hal::spi::config::DriverConfig::new().dma(Dma::Auto(512)),
+    ).unwrap();
+
+    let spi_bus = esp_idf_hal::spi::SpiBusDriver::new(
+        driver,
+        &esp_idf_hal::spi::SpiConfig::new().baudrate(3_200.kHz().into()),
+    ).unwrap();
+
+    let led_driver = ws2812_spi::Ws2812::new(spi_bus);
 }
