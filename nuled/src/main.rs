a1a237e6700ccfef0e09a9e0cd24cdffa18c70fc
Kim Tore Jensen <kimtjen@gmail.com>
2024-09-27 15:10:15 +0200

    nuled: working and tested onboard ws2812 led

--- nuled/src/main.rs
+++ nuled/src/main.rs
@@ -1,8 +1,13 @@
 #![no_std]
 #![no_main]
 
+use esp_idf_hal::gpio::AnyIOPin;
 use esp_idf_hal::prelude::{FromValueType, Peripherals};
-use esp_idf_hal::spi::Dma;
+use esp_idf_hal::spi::{Dma, SpiBusDriver, SpiDriver};
+use esp_idf_hal::spi::config::Config;
+use smart_leds::hsv::{hsv2rgb, Hsv};
+use smart_leds::{brightness, SmartLedsWrite, RGB8};
+use ws2812_spi::Ws2812;
 
 #[no_mangle]
 unsafe fn main() {
@@ -16,17 +21,34 @@ unsafe fn main() {
     log::info!("Hello, world!");
 
     let peripherals = Peripherals::take().unwrap();
-    let driver = esp_idf_hal::spi::SpiDriver::new_without_sclk(
+    let driver = SpiDriver::new_without_sclk(
         peripherals.spi2,
-        peripherals.pins.gpio10,
-        Option::<esp_idf_hal::gpio::AnyIOPin>::None,
+        peripherals.pins.gpio8,
+        Option::<AnyIOPin>::None,
         &esp_idf_hal::spi::config::DriverConfig::new().dma(Dma::Auto(512)),
     ).unwrap();
 
-    let spi_bus = esp_idf_hal::spi::SpiBusDriver::new(
-        driver,
-        &esp_idf_hal::spi::SpiConfig::new().baudrate(3_200.kHz().into()),
-    ).unwrap();
+    led_task(driver);
+}
 
-    let led_driver = ws2812_spi::Ws2812::new(spi_bus);
+#[derive(Debug)]
+pub enum Error {}
+
+pub fn led_task(driver: SpiDriver) {
+    let bus = SpiBusDriver::new(driver, &Config::new().baudrate(3_200.kHz().into())).unwrap();
+    let mut ws = Ws2812::new(bus);
+    let mut data = [RGB8::default(); 1];
+    #[allow(clippy::infinite_iter)]
+    (0..=255).cycle().for_each(|hue| {
+        unsafe {
+            esp_idf_sys::usleep(10_000);
+        }
+        data[0] = hsv2rgb(Hsv {
+            hue,
+            sat: 255,
+            val: 120,
+        });
+        let pixel = brightness(data.iter().cloned(), 30);
+        ws.write(pixel).unwrap();
+    });
 }
