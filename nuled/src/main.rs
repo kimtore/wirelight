81a02020715c5211ef9cee4b4082bc6622de59cf
Kim Tore Jensen <kimtjen@gmail.com>
2024-10-01 18:02:03 +0200

    Working LED blinker with esp32 and rust

--- nuled/src/main.rs
+++ nuled/src/main.rs
@@ -4,12 +4,13 @@
 use esp_idf_hal::gpio::AnyIOPin;
 use esp_idf_hal::peripheral::Peripheral;
 use esp_idf_hal::prelude::{FromValueType, Peripherals};
-use esp_idf_hal::spi::{Dma, SpiBusDriver, SpiDriver};
 use esp_idf_hal::spi::config::Config;
-use esp_idf_sys::{useconds_t, usleep};
+use esp_idf_hal::spi::{Dma, SpiBusDriver, SpiDriver};
+use esp_idf_sys::{usleep};
 use smart_leds::hsv::{hsv2rgb, Hsv};
-use smart_leds::{brightness, SmartLedsWrite, RGB8};
+use smart_leds::{RGB8};
 use ws2812_spi::Ws2812;
+use crate::intervals::SECOND;
 
 pub mod intervals {
     use esp_idf_sys::useconds_t;
@@ -36,10 +37,21 @@ unsafe fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    log::info!("NULED booted!");
 
     let peripherals = Peripherals::take().unwrap();
     let spi2 = peripherals.spi2.into_ref();
+
+    /*
+    let mut power_config = esp_idf_svc::sys::esp_pm_config_esp32c3_t {
+        max_freq_mhz: 160,
+        min_freq_mhz: 160,
+        light_sleep_enable: false,
+    };
+
+    let result = esp!(unsafe { esp_idf_svc::sys::esp_pm_configure(&mut power_config as *mut _ as *const c_void) });
+    result.unwrap();
+     */
 
     let driver = SpiDriver::new_without_sclk(
         spi2,
@@ -53,66 +65,73 @@ unsafe fn main() {
         &Config::new().baudrate(3_200.kHz().into()),
     ).unwrap();
 
+    // LED writer
     let mut ws = Ws2812::new(spi_bus);
 
-    // Boot sequence
-    {
-        led_blink(
-            &mut ws,
-            RGB8::new(0, 255, 0),
-            RGB8::new(0, 0, 0),
-            intervals::SECOND,
-            intervals::TWELWTH_SECOND,
-            1,
-        );
-    }
+    // Boot sequence finished
+    led::fill(&mut ws, RGB8::new(0, 255, 0));
+    usleep(SECOND);
 
+    // Test program: blink through each color hue
     loop {
         for hue in 0..=255 {
-            led_blink(
+            led::blink(
                 &mut ws,
                 hsv2rgb(Hsv { hue, sat: 255, val: 120 }),
                 hsv2rgb(Hsv { hue, sat: 255, val: 30 }),
-                intervals::TWELWTH_SECOND,
-                intervals::TWELWTH_SECOND,
+                intervals::QUARTER_SECOND,
+                intervals::QUARTER_SECOND,
                 1,
             );
         }
     }
 }
 
-pub unsafe fn led_blink<'a>(
-    ws: &mut Ws2812<SpiBusDriver<'a, SpiDriver<'a>>>,
-    on_color: RGB8,
-    off_color: RGB8,
-    duty_cycle_on_usec: useconds_t,
-    duty_cycle_off_usec: useconds_t,
-    cycles: usize,
-) {
-    let on = [on_color; LED_COUNT];
-    let off = [off_color; LED_COUNT];
+pub mod led {
+    use esp_idf_hal::spi::{SpiBusDriver, SpiDriver};
+    use esp_idf_sys::{useconds_t, usleep};
+    use smart_leds::{SmartLedsWrite, RGB8};
+    use smart_leds::hsv::{hsv2rgb, Hsv};
+    use ws2812_spi::Ws2812;
+    use crate::{intervals, LED_COUNT};
 
-    for _ in 0..cycles {
+    pub unsafe fn fill<'a>(
+        ws: &mut Ws2812<SpiBusDriver<'a, SpiDriver<'a>>>,
+        color: RGB8,
+    ) {
+        let on = [color; LED_COUNT];
         ws.write(on).unwrap();
-        usleep(duty_cycle_on_usec);
-        ws.write(off).unwrap();
-        usleep(duty_cycle_off_usec);
     }
-}
 
-pub fn rainbow_cycle<'a>(ws: &mut Ws2812<SpiBusDriver<'a, SpiDriver<'a>>>) {
-    let mut data = [RGB8::default(); 1];
-    #[allow(clippy::infinite_iter)]
-    (0..=255).cycle().for_each(|hue| {
-        unsafe {
-            usleep(10_000);
+    pub unsafe fn blink<'a>(
+        ws: &mut Ws2812<SpiBusDriver<'a, SpiDriver<'a>>>,
+        on_color: RGB8,
+        off_color: RGB8,
+        duty_cycle_on_usec: useconds_t,
+        duty_cycle_off_usec: useconds_t,
+        cycles: usize,
+    ) {
+        let on = [on_color; LED_COUNT];
+        let off = [off_color; LED_COUNT];
+
+        for _ in 0..cycles {
+            ws.write(on).unwrap();
+            usleep(duty_cycle_on_usec);
+            ws.write(off).unwrap();
+            usleep(duty_cycle_off_usec);
         }
-        data[0] = hsv2rgb(Hsv {
-            hue,
-            sat: 255,
-            val: 120,
-        });
-        let pixel = brightness(data.iter().cloned(), 30);
-        ws.write(pixel).unwrap();
-    });
+    }
+
+    pub unsafe fn test_program<'a>(ws: &mut Ws2812<SpiBusDriver<'a, SpiDriver<'a>>>) {
+        for hue in 0..=255 {
+            blink(
+                ws,
+                hsv2rgb(Hsv { hue, sat: 255, val: 120 }),
+                hsv2rgb(Hsv { hue, sat: 255, val: 30 }),
+                intervals::QUARTER_SECOND,
+                intervals::QUARTER_SECOND,
+                1,
+            );
+        }
+    }
 }
