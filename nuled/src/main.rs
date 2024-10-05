0100fe52730daa09cbeb185a38311549da5d1385
Kim Tore Jensen <kimtjen@gmail.com>
2024-10-05 13:43:18 +0200

    nuled: working no_std multitasked wifi and LED

--- nuled/src/main.rs
+++ nuled/src/main.rs
@@ -1,31 +1,41 @@
 #![no_std]
 #![no_main]
 
+use embassy_executor::Spawner;
 use esp_backtrace as _;
-use esp_hal::clock::ClockControl;
-use esp_hal::delay::Delay;
-use esp_hal::dma::DmaPriority;
-use esp_hal::gpio::Io;
-use esp_hal::peripherals::Peripherals;
+use esp_hal::clock::{ClockControl, Clocks};
+use esp_hal::dma::{DmaPriority};
+use esp_hal::gpio::{GpioPin, Io};
+use esp_hal::peripherals::{Peripherals, SPI2};
 use esp_hal::prelude::*;
 use esp_hal::riscv::_export::critical_section;
 use esp_hal::rng::Rng;
 use esp_hal::spi::SpiMode;
 use esp_hal::system::SystemControl;
-use esp_hal::timer::timg::TimerGroup;
+use esp_hal::timer::timg::{TimerGroup};
 use smart_leds::hsv::Hsv;
 use smart_leds::SmartLedsWrite;
-use ws2812_spi::Ws2812;
+use static_cell::StaticCell;
+use ws2812_spi::prerendered::Ws2812;
 
-#[allow(dead_code)]
 const WIFI_SSID: &'static str = env!("NULED_WIFI_SSID");
-#[allow(dead_code)]
 const WIFI_PASSWORD: &'static str = env!("NULED_WIFI_PASSWORD");
-#[allow(dead_code)]
 const LED_COUNT: usize = 60;
 
-#[no_mangle]
-fn main() {
+static CLOCKS: StaticCell<Clocks> = StaticCell::new();
+
+#[embassy_executor::task]
+async fn task() {
+    let mut i = 0;
+    loop {
+        i = i + 1;
+        log::info!("Task running [#{i}]...");
+        embassy_time::Timer::after_secs(1).await;
+    }
+}
+
+#[main]
+async fn main(spawner: Spawner) {
     esp_println::logger::init_logger(log::LevelFilter::Info);
 
     log::info!("NULED booting.");
@@ -34,53 +44,42 @@ fn main() {
     let system = SystemControl::new(peripherals.SYSTEM);
     let clocks = ClockControl::max(system.clock_control).freeze();
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let dma = esp_hal::dma::Dma::new(peripherals.DMA);
-    let dma_channel_0 = dma.channel0.configure(true, DmaPriority::Priority9);
-    let delay = Delay::new(&clocks);
 
-    let (
-        tx_buffer,
-        tx_descriptors,
-        rx_buffer,
-        rx_descriptors
-    ) = esp_hal::dma_buffers!(512);
+    let clocks: &'static mut Clocks = CLOCKS.init(clocks);
 
-    log::info!("Setting up DMA buffers.");
+    let embassy_timer = TimerGroup::new(peripherals.TIMG0, clocks);
 
-    let tx_dma = esp_hal::dma::DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
-    let rx_dma = esp_hal::dma::DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+    log::info!("Initializing embassy...");
+    esp_hal_embassy::init(clocks, embassy_timer.timer0);
 
-    log::info!("Initializing SPI driver at 3.2MHz");
+    log::info!("Spawning bullshit task...");
+    spawner.must_spawn(task());
+    log::info!("Spawning WiFi task...");
+    spawner.must_spawn(wifi_task(peripherals.TIMG1, peripherals.RNG, peripherals.RADIO_CLK, peripherals.WIFI, clocks));
+    log::info!("Spawning LED task...");
+    spawner.must_spawn(led_task(peripherals.SPI2, io.pins.gpio8, peripherals.DMA, clocks));
 
-    let spi = esp_hal::spi::master::Spi::new(
-        peripherals.SPI2,
-        3_200.kHz(),
-        SpiMode::Mode1,
-        &clocks,
-    )
-        .with_mosi(io.pins.gpio8)
-        .with_dma(dma_channel_0)
-        ;
+    loop {
+        embassy_time::Timer::after_secs(1).await;
+    }
+}
 
-    log::info!("Initializing SPI DMA bus...");
-
-    let spi_driver = esp_hal::spi::master::SpiDmaBus::new(spi, tx_dma, rx_dma);
-
-    let mut ws = Ws2812::new(spi_driver);
-
-    log::info!("WS2812 driver started on SPI2 and GPIO8.");
-
-    let wifi_timer = TimerGroup::new(peripherals.TIMG1, &clocks).timer0;
-    //let wifi_timer = PeriodicTimer::new(SystemTimer::new(peripherals.SYSTIMER).alarm0.into());
-    let wifi_rng = Rng::new(peripherals.RNG);
-
+#[embassy_executor::task]
+async fn wifi_task(
+    wifi_timer: esp_hal::peripherals::TIMG1,
+    rng: esp_hal::peripherals::RNG,
+    radio_clk: esp_hal::peripherals::RADIO_CLK,
+    wifi: esp_hal::peripherals::WIFI,
+    clocks: &'static Clocks<'static>,
+) {
     log::info!("Initializing WiFi configuration.");
 
+    let wifi_timer = TimerGroup::new(wifi_timer, clocks);
     let wifi_init = esp_wifi::initialize(
         esp_wifi::EspWifiInitFor::Wifi,
-        wifi_timer,
-        wifi_rng,
-        peripherals.RADIO_CLK,
+        wifi_timer.timer0,
+        Rng::new(rng),
+        radio_clk,
         &clocks,
     );
 
@@ -103,19 +102,64 @@ fn main() {
     let (_wifi_device, mut wifi_controller) =
         esp_wifi::wifi::new_with_config::<esp_wifi::wifi::WifiStaDevice>(
             &wifi_init,
-            peripherals.WIFI,
+            wifi,
             wifi_config,
         ).unwrap();
 
     log::info!("Starting WiFi controller.");
 
-    wifi_controller.start().unwrap();
+    wifi_controller.start().await.unwrap();
 
     log::info!("Connecting WiFi...");
 
-    wifi_controller.connect().unwrap();
+    match wifi_controller.connect().await {
+        Ok(_) => {
+            log::info!("WiFi connect success.");
+        }
+        Err(err) => {
+            log::error!("WiFi connect error: {:?}", err);
+        }
+    }
+}
 
-    log::info!("WiFi connect started.");
+#[embassy_executor::task]
+async fn led_task(spi: SPI2, pin: GpioPin<8>, dma: esp_hal::peripherals::DMA, clocks: &'static Clocks<'static>) {
+    log::info!("Setting up DMA buffers.");
+
+    let (
+        tx_buffer,
+        tx_descriptors,
+        rx_buffer,
+        rx_descriptors
+    ) = esp_hal::dma_buffers!(512);
+
+    let tx_dma = esp_hal::dma::DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+    let rx_dma = esp_hal::dma::DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+
+    log::info!("Initializing SPI driver at 3.2MHz");
+
+    let dma = esp_hal::dma::Dma::new(dma);
+    let dma_channel_0 = dma.channel0.configure(true, DmaPriority::Priority9);
+    let spi = esp_hal::spi::master::Spi::new(
+        spi,
+        3_200.kHz(),
+        SpiMode::Mode0,
+        &clocks,
+    )
+        .with_mosi(pin)
+        .with_dma(dma_channel_0)
+        ;
+
+    log::info!("Initializing SPI DMA bus...");
+
+    let spi_driver = esp_hal::spi::master::SpiDmaBus::new(spi, tx_dma, rx_dma);
+
+    let mut led_buffer = [0_u8; (LED_COUNT * 12) + 40];
+    let mut ws = Ws2812::new(spi_driver, &mut led_buffer);
+
+    log::info!("WS2812 driver started on SPI2 and GPIO8.");
+
+    embassy_time::Timer::after_millis(1).await;
 
     loop {
         let sat = 255;
@@ -126,8 +170,8 @@ fn main() {
                 critical_section::with(|_| {
                     ws.write(data).unwrap();
                 });
-                delay.delay_millis(50);
             }
+            embassy_time::Timer::after_millis(50).await;
         }
     }
 }
